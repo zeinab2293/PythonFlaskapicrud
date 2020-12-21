0d86c723627141f3c5fbbc0aab048cddb63bbfda
Zeinab M.Ali <Zeinab2293@gmail.com>
2020-12-21 18:20:11 +0200

    modeldatabase

---
+++ model.py
@@ -0,0 +1,10 @@
+
+#create database test
+
+#CREATE TABLE `tbl_user` (
+  #`user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  #`user_name` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  #`user_email` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+ 
+ # PRIMARY KEY (`user_id`)
+#) ;
